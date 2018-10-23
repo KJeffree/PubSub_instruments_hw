c8bfc6575875c188aa1e487fc9929e92edcd5ba4
Katie Jeffree <k.jeffree@gmail.com>
2018-10-23 17:30:43 +0100

    instrument view added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const instrumentData = require('./data/instrument_families.js')
 const InstrumentFamilies = require('./models/instrument_families.js')
 const SelectInstrumentView = require('./views/select_instrument_view.js')
+const InstrumentInfoView = require('./views/instrument_info_view.js')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
@@ -11,4 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const instrumentDataModel = new InstrumentFamilies(instrumentData);
   instrumentDataModel.bindEvents();
+
+  const infoSec = document.querySelector('sec')
 });
--- src/views/instrument_info_view.js
+++ src/views/instrument_info_view.js
@@ -1,1 +1,21 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const InstrumentInfoView = function (container){
+  this.container = container;
+}
+
+InstrumentInfoView.prototype.bindEvents = function(){
+  PubSub.subscribe('Instruments:selected-instrument-ready', (event) => {
+    const instrument = event.detail;
+    this.render(instrument);
+  })
+}
+
+InstrumentInfoView.prototype.render = function(instrument){
+  const infoInstrumentName = document.createElement('h1');
+  infoInstrumentName.textContent = `${ instrument.name } `;
+  this.container.innerHTML = "";
+  this.container.appendChild(infoInstrumentName)
+}
+
+module.exports = InstrumentInfoView;
